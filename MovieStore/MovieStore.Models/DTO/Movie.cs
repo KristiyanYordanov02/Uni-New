cd4030b3959416ab2caee78d0dbc8014bebfcd52
KristiyanYordanov02 <kingroalbg@gmail.com>
2024-12-16 17:40:30 +0200

    update

---
+++ MovieStore/MovieStore.Models/DTO/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieStore.Models.DTO
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public int Year { get; set; }
+
+        public IEnumerable<int> Actors { get; set; } =
+            Enumerable.Empty<int>();
+    }
+}
